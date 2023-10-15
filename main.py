ad11eae171190cf5e1bc9dbcb49d3a6b4ce1c6e1
roma_z <35759448+romaz21@users.noreply.github.com>
2023-10-15 18:02:44 +0300

    fs

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import math
+from phe import paillier
+public_key, private_key = paillier.generate_paillier_keypair()
+secret_number_list = [3.141592653, 300, -4.6e-12]
+encrypted_number_list = [public_key.encrypt(x) for x in secret_number_list]
+print([private_key.decrypt(x) for x in encrypted_number_list])
+
+a, b, c = encrypted_number_list
+print(private_key.decrypt(a+b) == secret_number_list[0] + secret_number_list[1])
+
+print(public_key, private_key)
